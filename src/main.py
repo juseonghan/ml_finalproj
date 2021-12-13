61c1589607b00a76e432267af32b5587a42aad3a
John Han <jhan62@jh.edu>
2021-12-12 22:13:29 -0500

    made a main to test conv2d

--- src/main.py
+++ src/main.py
@@ -1,1 +1,29 @@
-print('hi')
+import numpy as np
+import cv2 as cv
+from numpy.lib.arraypad import pad
+import matplotlib.pyplot as plt
+import time
+
+print(cv.__version__)
+print(np.__version__)
+
+mask = np.ones((5,5)) / 25
+
+img = cv.imread('../girl.png', 0)
+
+img_numpy = np.asarray(img)
+
+start = time.time()
+
+output = conv2d(img_numpy, mask)
+
+end = time.time()
+
+print(str(end-start) + ' seconds')
+
+plt.figure(1)
+plt.imshow(img,cmap='gray')
+
+plt.figure(2)
+plt.imshow(output,cmap='gray')
+
